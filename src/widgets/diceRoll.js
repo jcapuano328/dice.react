7bcbc73ca2f079902736f98a76eb5b5c7f14ce5b
Jeff Capuano <jcapuano328@gmail.com>
2016-10-07 07:05:33 -0400

    tweak dice roll widget layout

--- src/widgets/diceRoll.js
+++ src/widgets/diceRoll.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var React = require('react');
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 var Button = require('apsl-react-native-button');
 var Dice = require('../services/dice');
 var DieButton = require('./die');
@@ -18,6 +18,7 @@ var DiceRoll = React.createClass({
       this.props.onDie && this.props.onDie(e, die.value());
     },
     render() {
+        let size = this.props.size || 64;
         this.dice = new Dice.Dice(this.props.dice);
         return (
             <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center'}}>
@@ -27,18 +28,27 @@ var DiceRoll = React.createClass({
                     }
                     return (
                         <View key={i} style={{flex:1}}>
-                            <DieButton die={i+1} value={die.value()} color={die.color()} onPress={this.onDie} />
+                            <DieButton die={i+1} value={die.value()} size={size} color={die.color()} onPress={this.onDie} />
                         </View>
                     );
                 })}
-                <View style={{flex:1}}>
-                    <Button
-                        style={{width: this.props.buttonWidth || 96,height: this.props.buttonHeight || 64,
-                                marginTop: 5,
-                                backgroundColor: this.props.buttonBackgroundColor || '#3F51B5'}}
-                        textStyle={{fontSize: 18,color: this.props.buttonColor || '#FFF'}} onPress={this.onRoll}>
-                        {'Roll'}
-                    </Button>
+                <View style={{flex:1, marginRight: 5, justifyContent: 'center'}}>
+                    <TouchableOpacity onPress={this.onRoll} style={{
+                        //flex: 1,
+                        //flexDirection: 'row',
+                        justifyContent: 'center',
+                        //width: size,
+                        height: size,
+                        backgroundColor: this.props.buttonBackgroundColor || '#3F51B5',
+                        borderRadius: 10,
+                        borderColor: 'black',
+                        borderWidth: 1,
+                        //marginTop: 5,
+                        //marginLeft: 5,
+                        //marginRight: 5
+                    }}>
+                        <Text style={{fontSize: 18,color: this.props.buttonColor || '#FFF', alignSelf: 'center', textAlign: 'center'}}>Roll</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         );
