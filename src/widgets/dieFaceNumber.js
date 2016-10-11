24974bdc6db8413d0f35caf4d6257775ffac78aa
Jeff Capuano <jcapuano328@gmail.com>
2016-10-11 06:37:55 -0400

    alter font for 10 sided die

--- src/widgets/dieFaceNumber.js
+++ src/widgets/dieFaceNumber.js
@@ -5,7 +5,7 @@ import { View, Text } from 'react-native';
 
 var DieFace = React.createClass({
     render() {
-        let size = (this.props.size || 64) * .75;
+        let size = (this.props.size || 64) * .65;
         return (
             <View style={{flex:1, justifyContent: 'center'}}>
                 <Text style={{fontSize: size, fontWeight: 'bold', fontFamily:'sans-serif-black', color: this.props.color || 'black', textAlign:'center', alignSelf:'center'}}>
