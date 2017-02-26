926413dc2fa62b189ef6c1552e934e8aa9e173a1
Jeff Capuano <jcapuano328@gmail.com>
2017-02-26 11:02:28 -0500

    tweak the roll button

--- src/widgets/diceRoll.js
+++ src/widgets/diceRoll.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import Style from '../services/style';
 import Dice from '../services/dice';
 import Die6 from './die6Sided';
 import Die10 from './die10Sided';
+var DiceRollIcon = require('../resources/dicecup.png');
 
 var RollButton = React.createClass({
+    getInitialState() {
+        return {
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0,
+            viewHeight: 100
+        };
+    },
+    onLayout(e) {
+        if (this.state.width != e.nativeEvent.layout.width ||
+            this.state.height != e.nativeEvent.layout.height) {
+            this.setState({
+                x: e.nativeEvent.layout.x,
+                y: e.nativeEvent.layout.y,
+                width: e.nativeEvent.layout.width,
+                height: e.nativeEvent.layout.height
+            });
+        }
+    },
     render() {
-        let paddingLeft = this.props.direction == 'vertical' ? 30 : 10;
-        let paddingRight = this.props.direction == 'vertical' ? 30 : 10;
+        let width = (this.state.width*0.9) || 32;
+        let height = (this.state.height*0.9) || 32;
+
+        let paddingLeft = this.props.direction == 'vertical' ? 45 : 15;
+        let paddingRight = this.props.direction == 'vertical' ? 45 : 15;
         return (
             <TouchableOpacity onPress={this.props.onRoll} style={{
                 justifyContent: 'center',
-                backgroundColor: this.props.buttonBackgroundColor || '#3F51B5',
+                backgroundColor: this.props.buttonBackgroundColor || 'silver',//'#3F51B5',
                 borderRadius: 10,
                 borderColor: 'black',
                 borderWidth: 1,
-                paddingTop: 10,
-                paddingLeft: paddingLeft,
-                paddingRight: paddingRight,
-                paddingBottom: 10
-            }}>
-                <Text style={{/*fontSize: 18,*/color: this.props.buttonColor || '#FFF', alignSelf: 'center', textAlign: 'center'}}>Roll</Text>
+                paddingTop: Style.Padding.pad(15),
+                paddingLeft: Style.Padding.pad(paddingLeft),
+                paddingRight: Style.Padding.pad(paddingRight),
+                paddingBottom: Style.Padding.pad(15)
+            }} onLayout={this.onLayout}>
+                {/*<Text style={{fontSize: Style.Font.medium(),color: this.props.buttonColor || '#FFF', alignSelf: 'center', textAlign: 'center'}}>Roll</Text>*/}
+                <Image
+                    style={{width: width, height: height, resizeMode: 'contain', alignSelf: 'center'}}
+                    source={DiceRollIcon} />
+
             </TouchableOpacity>
         );
     }
