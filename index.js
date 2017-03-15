edaae69019c0651c1a06ef19dfd23a1637a0f0ba
Jeff Capuano <jcapuano328@gmail.com>
2017-03-15 06:58:24 -0400

    export the button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     Dice: require('./src/services/dice').Dice,    
     DiceTray: require('./src/widgets/diceTray'),
+	RollButton: require('./src/widgets/rollButton'),
     DiceRoll: require('./src/widgets/diceRoll')
 };
